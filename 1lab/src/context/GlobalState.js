2e80e18d37a3073b6c55a97051b50ff6bc25330c
DESKTOP-LLH4U6F\gaded <g.dedinaite@gmail.com>
2020-09-29 11:55:51 +0300

    start

---
+++ 1lab/src/context/GlobalState.js
@@ -0,0 +1,26 @@
+import React, {Children, createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+//State initiliazer
+
+const initialState = {
+    users: [
+        {id: 1, firstname: 'Michael', lastName: 'Putvin', job: 'Manager', deparment: 'Accounting'},
+        {id: 2, firstname: 'Alice', lastName: 'Wondervuna', job: 'Accountant', deparment: 'Accounting'},
+        {id: 3, firstname: 'Will', lastName: 'Tomskin', job: 'Manager', deparment: 'Public Relations'}
+    ]
+
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobaProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    return (
+        <GlobalContext.Provider value ={{
+            users: state.users
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
